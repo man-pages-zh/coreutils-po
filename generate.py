e856da056da1e59ca7d34535073890c1468df2e2
Darcy Shen <sadhen@zoho.com>
2015-04-26 11:38:43 +0800

    直接从手册页提取po

--- generate.py
+++ generate.py
@@ -1,18 +1,7 @@
 #!/usr/bin/env python3
 
 import os, re
-from subprocess import check_output, call
-
-docdir = "doc/"
-srcdir = docdir + "en/"
-tardir = docdir + "zh_CN/"
-suffix = ".md"
-
-if not os.path.exists(docdir):
-    os.mkdir(docdir)
-
-if not os.path.exists(srcdir):
-    os.mkdir(srcdir)
+from subprocess import check_output
 
 poconf = "[po4a_langs] zh_CN\n"
 poconf = poconf + "[po4a_paths] template/$master.pot $lang:po/$master.$lang.po\n"
@@ -22,27 +11,8 @@ for root, dirs, files in os.walk("raw"):
         for filename in files:
             if re.match(".*\.[1-9]", filename):
                 ifilepath = root+"/"+filename
-                ofilepath = srcdir + filename + suffix
-                
-
-                if not os.path.exists(ofilepath):
-                    print("[raw] Converting", ifilepath, "...",end="", flush=True)
-
-                    print("html...", end="", flush=True)
-                    html = check_output(["man2html", ifilepath], universal_newlines=True)
-
-                    print("markdown...", end="", flush=True)
-                    output = check_output(["pandoc","-f", "html", "-t", "markdown", "--no-wrap"],
-                                          universal_newlines=True, input=html)
-    
-                    print("write...", end="", flush=True)
-                    f = open(ofilepath, "w")
-                    f.write(output)
-                    f.close()
-                    print("done", flush=True)
-                    
-                poconf = poconf + "[type: text] " + ofilepath + " zh_CN:" 
-                poconf = poconf + ofilepath.replace(srcdir, tardir)
+                poconf = poconf + "[type: man] " + ifilepath + " zh_CN:" 
+                poconf = poconf + ifilepath.replace("raw", "src")
                 poconf = poconf + ' opt:"-L UTF-8 -M UTF-8"' + "\n"
 
 poconfpath = "/tmp/po.conf"
