4b215a725a79bac08470d6de0f463faf3da8ff73
Darcy Shen <sadhen@zoho.com>
2015-05-17 18:07:31 +0800

    Add Chinese Support(tmac) for generate.py

--- generate.py
+++ generate.py
@@ -20,3 +20,23 @@ poconffd = open(poconfpath, "w")
 poconffd.write(poconf)
 poconffd.close()
 print(check_output(["po4a", poconfpath], universal_newlines=True))
+
+# Add ". mso zh.tmac" for each translated file
+ChineseSupport = '. mso zh.tmac\n'
+
+for root, dirs, files in os.walk("src"):
+    if "man" in root:
+        for filename in files:
+            if re.match(".*\.[1-9]", filename):
+                ifilepath = root+"/"+filename
+                file = open(ifilepath, 'r')
+                firstLine = file.readline()
+                remainingLines = file.readlines()
+                file.close()
+                if firstLine != ChineseSupport:
+                    file = open(ifilepath, 'w')
+                    file.write(ChineseSupport)
+                    file.write(firstLine)
+                    file.write("".join(remainingLines))
+                    file.close()
+                    print("[newly translated] " + ifilepath)
